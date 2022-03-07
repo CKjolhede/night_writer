d19026d3a3c3982a1c37bad3560d0b616a0c9734
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-07 00:20:17 -0700

    test: write test to confirm the return of braille_reference set

--- spec/convert_to_braille_spec.rb
+++ spec/convert_to_braille_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe ConvertToBraille do
   it "exists" do
     expect(@braille).to be_a(ConvertToBraille)
   end
+
+  it 'can return the braille refernce set' do
+    expect(@braille.braille_reference['a']).to eq(["0.", "..", ".."])
+    expect(@braille.braille_reference['z']).to eq(["0.", ".0", "00"])
+  end
+
 end
