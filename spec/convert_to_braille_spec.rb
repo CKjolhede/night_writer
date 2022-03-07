28d5fd7c710d6bf7eb57928f95c5e69bea942a0e
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-07 01:18:43 -0700

    test: convert individual @characters to braille equivalent

--- spec/convert_to_braille_spec.rb
+++ spec/convert_to_braille_spec.rb
@@ -23,7 +23,22 @@ RSpec.describe ConvertToBraille do
   it 'can isolate individual characters from text' do
     expected = @braille.isolate(@incoming_text)
     #test message is "abc"
-    expect(expected).to eq(["a","b","c", "\n"])
+    expect(expected).to eq(["a", "b", "c", "\n"])
   end
-
 end
+
+  context "isolated characters" do
+    before :each do
+      @braille = ConvertToBraille.new
+        handle = File.open("test_message.txt", "r")
+        #test message text is:  abc
+        @incoming_text = handle.read
+        handle.close
+        @characters = @braille.isolate(@incoming_text)
+    end
+
+    it 'can change each @characters into their braille equivalent' do
+      expected = letter_to_braille(@characters)
+      expect(expected).to eq(["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."])
+    end
+  end
