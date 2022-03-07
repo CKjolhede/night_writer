e34e980838f3fcd74cf8cfac12e92a58febcf8fc
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-07 00:02:48 -0700

    feat: write reader that outputs text

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -6,10 +6,10 @@ handle.close
 
 puts "This is the incoming text #{incoming_text}"
 
-english = EnglishConverter.new
-
-writer = File.open(ARGV[1], "w")
-write.write(english.convert(incoming_text))
-write.close
-
-puts "Created #{ARGV[1]} containing #{incoming.text.length} characters"
+# english = EnglishConverter.new
+#
+# writer = File.open(ARGV[1], "w")
+# write.write(english.convert(incoming_text))
+# write.close
+#
+# puts "Created #{ARGV[1]} containing #{incoming.text.length} characters"
